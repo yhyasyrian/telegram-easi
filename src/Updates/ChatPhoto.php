c282967cd230b334303d567aa78c8c3915a11e0e
YhyaSyrian <yhya.syrian@gmail.com>
2023-10-09 12:38:11 +0300

    fix update ChatPhoto

--- src/Updates/ChatPhoto.php
+++ src/Updates/ChatPhoto.php
@@ -28,4 +28,34 @@ class ChatPhoto
      * @var string
      */
     public string $big_file_unique_id;
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     *
+     * @var string
+     */
+    public string $file_id;
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     *
+     * @var string
+     */
+    public string $file_unique_id;
+    /**
+     * Optional. File size in bytes
+     *
+     * @var int|null
+     */
+    public int|null $file_size;
+    /**
+     * Photo width
+     *
+     * @var int
+     */
+    public int $width;
+    /**
+     * Photo height
+     *
+     * @var int
+     */
+    public int $height;
 }
